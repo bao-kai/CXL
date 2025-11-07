255bb0b8d92496a04df6fe076f50565e492d7a10
BAO-KAI, WANG <138642029+bao-kai@users.noreply.github.com>
2025-11-07 17:15:13 +0800

    Add BS.c for memory allocation and initialization

---
+++ benchmark/BS.c
@@ -0,0 +1,43 @@
+// mmap_read_64g.c
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main() {
+    size_t file_size = 128ULL * 1024 * 1024 * 10; // 128MB
+    off_t one_thread = 64ULL * 1024 * 1024 ; // 64MB
+
+    off_t one_thread_numbers = one_thread / sizeof(uint64_t); // 每個線程寫入的 uint64_t 數量
+    off_t rep_times = file_size / one_thread_numbers; // 
+
+    printf("Allocating %zu bytes (%.1f GB)\n",
+           file_size, file_size / (1024.0 * 1024.0 * 1024.0));
+
+    volatile uint8_t *p = (uint64_t *)malloc(file_size);
+    if (!p) {
+        fprintf(stderr, "malloc() failed\n");
+        return EXIT_FAILURE;
+    }
+    printf("rep_times: %d, one_thread_numbers: %d\n", (int)rep_times, (int)one_thread_numbers);
+    // 初始化記憶體 (確保實際分配到實體頁面)
+    for(size_t j = 0; j < rep_times ;j++){
+        for (size_t i = 0; i < one_thread_numbers; i++) {
+            p[one_thread_numbers * j + i] = (uint64_t)(i % 256);
+        }
+        usleep(1);
+        
+    }
+
+    
+
+    
+
+    free(p);
+    return EXIT_SUCCESS;
+}
