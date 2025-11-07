3924fc41d0b00e2a48ae8f97e3176d7e914b27f6
BAO-KAI, WANG <138642029+bao-kai@users.noreply.github.com>
2025-11-07 16:50:56 +0800

    Fix one_thread size and improve memory initialization
    Adjust one_thread size and update memory initialization loop to use rep_times and one_thread_numbers.

--- benchmark/SPMV.c
+++ benchmark/SPMV.c
@@ -11,7 +11,7 @@
 
 int main() {
     size_t file_size = 638ULL * 1024 * 1024; // 128GB
-    off_t one_thread = 512ULL * 1024 * 1024; // 64MB
+    off_t one_thread = 64ULL * 1024 * 1024 * 256; // 64MB
 
     off_t one_thread_numbers = one_thread / sizeof(uint32_t); // 每個線程寫入的 uint64_t 數量
     off_t rep_times = file_size / one_thread_numbers; // 
@@ -26,9 +26,9 @@ int main() {
     }
     printf("rep_times: %d, one_thread_numbers: %d\n", (int)rep_times, (int)one_thread_numbers);
     // 初始化記憶體 (確保實際分配到實體頁面)
-    for(size_t j = 0; j < 1 ;j++){
-        for (size_t i = 0; i < file_size; i++) {
-            p[file_size * j + i] = (uint8_t)(i % 256);
+    for(size_t j = 0; j < rep_times ;j++){
+        for (size_t i = 0; i < one_thread_numbers; i++) {
+            p[file_size * j + i] = (uint32_t)(i % 256);
         }
         usleep(1);
         
